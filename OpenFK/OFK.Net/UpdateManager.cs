ab828032937c8c7d4e9c5efd68ce7e57372e59d6
NanderTGA <nander.roobaert@gmail.com>
2026-04-17 14:52:25 +0200

    refactor: reduce indentation and simplify some code

--- OpenFK/OFK.Net/UpdateManager.cs
+++ OpenFK/OFK.Net/UpdateManager.cs
@@ -3,6 +3,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
+using System.Reflection;
 using System.Xml.Linq;
 
 namespace OpenFK.OFK.Net
@@ -16,36 +17,34 @@ namespace OpenFK.OFK.Net
         static XDocument UpdateStore;
         static XDocument FSUpdateStore;
 
+        public static string RemoveBuildNumberFromVersion(string version)
+        {
+            return version.Substring(0, version.LastIndexOf("."));
+        }
+
         /// <summary>
         /// Checks a remote XML store to find an update for OpenFK and FunkeySelectorGUI.
         /// </summary>
         public static void CheckUpdate()
         {
             //OpenFK version
-            string localVerNum = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
-            localVerNum = localVerNum.Substring(0, localVerNum.LastIndexOf("."));
-            string localVersion = $"OpenFK v{localVerNum}";
+            string currentOFKVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            currentOFKVersion = RemoveBuildNumberFromVersion(currentOFKVersion);
+            string localVersion = $"OpenFK v{currentOFKVersion}";
 
             //FSGUI version - Checks the new and old name, favors the new one.
             bool isFSGUIHere = false;
             string fslocalVerNum = "";
             string fslocalVersion = "";
-            if (File.Exists(Directory.GetCurrentDirectory() + @"\FunkeySelectorGUI.exe"))
+            
+            string[] fsNames = { "FunkeySelectorGUI.exe", "FunkeySelector.exe" };
+            foreach (string file in fsNames)
             {
-                isFSGUIHere = true;
-                fslocalVerNum = FileVersionInfo.GetVersionInfo(Directory.GetCurrentDirectory() + @"\FunkeySelectorGUI.exe").FileVersion;
-                fslocalVerNum = localVerNum.Substring(0, localVerNum.LastIndexOf("."));
-            }
-            else if (File.Exists(Directory.GetCurrentDirectory() + @"\FunkeySelector.exe"))
-            {
-                isFSGUIHere = true;
-                fslocalVerNum = FileVersionInfo.GetVersionInfo(Directory.GetCurrentDirectory() + @"\FunkeySelector.exe").FileVersion;
-                fslocalVerNum = localVerNum.Substring(0, localVerNum.LastIndexOf("."));
-            }
+                if (!File.Exists(file)) continue;
 
-            if (isFSGUIHere)
-            {
+                fslocalVerNum = FileVersionInfo.GetVersionInfo(Path.GetFullPath(file)).FileVersion;
                 fslocalVersion = $"FunkeySelectorGUI v{fslocalVerNum}";
+                break;
             }
 
             LogManager.LogNetwork("[Update] Update Requested", "NetCommand");
@@ -61,58 +60,58 @@ namespace OpenFK.OFK.Net
                 string netVersionNum = UpdateStore.Root.Attribute("version").Value;
                 string netVersionSize = UpdateStore.Root.Attribute("size").Value;
                 Globals.GameForm.SetVar(@"<progress percent=""50.00"" />");
-                if (localVerNum != netVersionNum)
+                if (currentOFKVersion != netVersionNum)
                 {
                     LogManager.LogNetwork("[Update] An update is needed", "NetCommand");
                     UpdateStore.Save(Directory.GetCurrentDirectory() + @"\update.xml");
-                    Globals.GameForm.SetVar(@"<checkupdate result=""2"" reason=""New version of OpenFK found."" version=""2009_07_16_544"" size=""" + netVersionSize + @""" curversion=""" + localVerNum + @""" extversion=""" + netVersionNum + @""" extname=""" + netVersion + @""" />");
-                }
-                else if (isFSGUIHere)
-                {
-                    Globals.GameForm.SetVar(@"<progress percent=""75.00"" />");
-                    try
-                    {
-                        LogManager.LogNetwork("[Update] Downloading FSGUI Update.xml from Codeberg", "NetCommand");
-                        FSUpdateStore = XDocument.Parse(HttpManager.HTTPGet(@"https://codeberg.org/OpenFunk/FunkeySelectorGUI/raw/branch/main/update.xml"));
-                        LogManager.LogNetwork("[Update] FSGUI Update.xml was downloaded", "NetCommand");
-                        string fsnetVersion = FSUpdateStore.Root.Attribute("name").Value;
-                        string fsnetVersionNum = FSUpdateStore.Root.Attribute("version").Value;
-                        string fsnetVersionSize = FSUpdateStore.Root.Attribute("size").Value;
-                        Globals.GameForm.SetVar(@"<progress percent=""90.00"" />");
-                        if (fslocalVerNum != fsnetVersionNum)
-                        {
-                            try
-                            {
-                                Process process = Process.GetProcessesByName("FunkeySelectorGUI")[0];
-                                process.Kill();
-                            }
-                            catch
-                            {
-                                LogManager.LogNetwork("[Update] Cannot close FSGUI", "NetCommand");
-                            }
-                            LogManager.LogNetwork("[Update] A FSGUI update is needed", "NetCommand");
-                            Globals.GameForm.SetVar(@"<checkupdate result=""2"" reason=""New version of FSGUI found."" version=""2009_07_16_544"" size=""" + fsnetVersionSize + @""" curversion=""" + fslocalVerNum + @""" extversion=""" + fsnetVersionNum + @""" extname=""" + fsnetVersion + @""" />");
-                        }
-                        else
-                        {
-                            Globals.GameForm.SetVar(@"<checkupdate result=""0"" reason=""Everything is up to date."" />");
-                        }
-                    }
-                    catch
-                    {
-                        LogManager.LogNetwork("[Update] [Error] Failed to check FSGUI update.", "NetCommand");
-                        Globals.GameForm.SetVar(@"<checkupdate result=""1"" reason=""Could not find the FunkeySelectorGUI update!"" />");
-                    }
-                }
-                else
-                {
-                    Globals.GameForm.SetVar(@"<checkupdate result=""0"" reason=""Everything is up to date."" />");
+                    Globals.GameForm.SetVar(@"<checkupdate result=""2"" reason=""New version of OpenFK found."" version=""2009_07_16_544"" size=""" + netVersionSize + @""" curversion=""" + currentOFKVersion + @""" extversion=""" + netVersionNum + @""" extname=""" + netVersion + @""" />");
+                    return;
                 }
             }
             catch
             {
                 LogManager.LogNetwork("[Update] [Error] Failed to check OpenFK update.", "NetCommand");
                 Globals.GameForm.SetVar(@"<checkupdate result=""1"" reason=""Could not find the OpenFK update!"" />");
+            }
+
+            if (!isFSGUIHere)
+            {
+                Globals.GameForm.SetVar(@"<checkupdate result=""0"" reason=""Everything is up to date."" />");
+                return;
+            }
+
+            Globals.GameForm.SetVar(@"<progress percent=""75.00"" />");
+            try
+            {
+                LogManager.LogNetwork("[Update] Downloading FSGUI Update.xml from Codeberg", "NetCommand");
+                FSUpdateStore = XDocument.Parse(HttpManager.HTTPGet(@"https://codeberg.org/OpenFunk/FunkeySelectorGUI/raw/branch/main/update.xml"));
+                LogManager.LogNetwork("[Update] FSGUI Update.xml was downloaded", "NetCommand");
+                string fsnetVersion = FSUpdateStore.Root.Attribute("name").Value;
+                string fsnetVersionNum = FSUpdateStore.Root.Attribute("version").Value;
+                string fsnetVersionSize = FSUpdateStore.Root.Attribute("size").Value;
+                Globals.GameForm.SetVar(@"<progress percent=""90.00"" />");
+                if (fslocalVerNum == fsnetVersionNum)
+                {
+                    Globals.GameForm.SetVar(@"<checkupdate result=""0"" reason=""Everything is up to date."" />");
+                    return;
+                }
+
+                try
+                {
+                    Process process = Process.GetProcessesByName("FunkeySelectorGUI")[0];
+                    process.Kill();
+                }
+                catch
+                {
+                    LogManager.LogNetwork("[Update] Cannot close FSGUI", "NetCommand");
+                }
+                LogManager.LogNetwork("[Update] A FSGUI update is needed", "NetCommand");
+                Globals.GameForm.SetVar(@"<checkupdate result=""2"" reason=""New version of FSGUI found."" version=""2009_07_16_544"" size=""" + fsnetVersionSize + @""" curversion=""" + fslocalVerNum + @""" extversion=""" + fsnetVersionNum + @""" extname=""" + fsnetVersion + @""" />");
+            }
+            catch
+            {
+                LogManager.LogNetwork("[Update] [Error] Failed to check FSGUI update.", "NetCommand");
+                Globals.GameForm.SetVar(@"<checkupdate result=""1"" reason=""Could not find the FunkeySelectorGUI update!"" />");
             }
         }
 
@@ -137,16 +136,8 @@ namespace OpenFK.OFK.Net
                 }
                 else
                 {
-                    string netDL = "";
-                    if (Environment.Is64BitProcess)
-                    {
-                        netDL = UpdateStore.Root.Attribute("url64").Value;
-                    }
-                    else
-                    {
-                        netDL = UpdateStore.Root.Attribute("url32").Value;
-                    }
-
+                    string netDL = UpdateStore.Root.Attribute(Environment.Is64BitProcess ? "url64" : "url32").Value;
+                    
                     using (var client = new WebClient())
                     {
                         ServicePointManager.Expect100Continue = true;
